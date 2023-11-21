ecdc2dc016e3136903ab13507db1f886f8d1af09
Shiyam DT <Shiyamdt8@gmail.com>
2023-11-21 22:19:17 +0530

    change

--- aeroproj/aerouser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AerouserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aerouser'
--- aeroproj/aerouser/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class userdata(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    phonenumber = models.IntegerField(null=True)
-    joindate = models.DateField(null=True)
