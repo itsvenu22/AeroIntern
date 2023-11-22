5d5162742b839a10bc180a51edcc8604f2bc726b
Venukanthan BS <114436303+itsvenu22@users.noreply.github.com>
2023-11-22 11:28:00 +0530

    Add files via upload

--- aeroproj/aerouser/models.py
+++ aeroproj/aerouser/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 # Create your models here.
 class userdata(models.Model):
-    username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
