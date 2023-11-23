bfe5804472a0b717fe43d387098c01cd5a6eed6c
Shiyam DT <141911803+Sub-tachyon@users.noreply.github.com>
2023-11-23 10:44:13 +0530

    Add files via upload

--- aeroproj/aerouser/views.py
+++ aeroproj/aerouser/views.py
@@ -17,21 +17,26 @@ def signup(request):
     return render(request, "signup.html")'''
 
 def signup(request):
+    error_message = None 
     if request.method == 'POST':
         # Assuming you receive data through a POST request
         field1_data = request.POST.get('username')  
         field2_data = request.POST.get('email')   
         field3_data = request.POST.get('password')
         # Create an instance of YourModel
-        
-        hashed_password=make_password(field3_data)
-        new_entry = userdata(username=field1_data, email=field2_data,password=hashed_password)
-
-        # Save the data to the database
-
-        return HttpResponse("success")
+       
+        if userdata.objects.filter(email=field2_data).exists():
+            error_message = "Email already exists in the database."
+            return render(request, "signup.html", {'error_message': error_message})
+        else:
+            hashed_password = make_password(field3_data)
+            new_entry = userdata(username=field1_data, email=field2_data, password=hashed_password)
+            new_entry.save()
+            return HttpResponse("Signup successful")
     else:
-        return render(request, "signup.html")
+        return render(request, "signup.html", {'error_message': error_message}) 
+
+      
 
 '''
 def details(request,id):
