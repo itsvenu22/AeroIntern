b2af1fabd7bf227235f2e66b6fc7aca5d7fc58c3
Venukanthan BS <itsvenu22@gmail.com>
2023-12-12 15:51:27 +0530

    feat(usergroup): log visibility for superuser

--- aeroproj/aeroproj/settings.py
+++ aeroproj/aeroproj/settings.py
@@ -43,8 +43,14 @@ INSTALLED_APPS = [
     'aerouser',
     'aerodevice',
     'rest_framework',
+    'channels',
 ]
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
